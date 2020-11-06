e84e45710284fd20d298bf78af9bcfb66e7c53f0
Hao Tran <hao.tran@go1.com>
2020-11-06 15:34:05 +0700

    Initial commit

---
+++ src/service.js
@@ -0,0 +1,15 @@
+export const getUsers = () => Promise.resolve([
+  {
+    id: 1,
+    name: 'Han',
+  },
+  {
+    id: 2,
+    name: 'Hoa',
+  },
+  {
+    id: 3,
+    name: 'Tuan Anh',
+  }
+])
+;
